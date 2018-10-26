e0aacb460419d6f678ca14ca0ea6cf0050cbf554
asmadi mohd <14b3370@ubd.edu.bn>
2018-10-27 00:22:01 +0800

    create meeting UI

--- resources/views/meeting/create.blade.php
+++ resources/views/meeting/create.blade.php
@@ -13,10 +13,38 @@
     <div class="row">
         <div class="col-md-10 offset-md-1">
             <div class="card">
-                <div class="card-header">Create Meeting</div>
+                <div class="card-header"><h3>Create Meeting</h3></div>
 
                 <div class="card-body">
-                    abc
+                   <form>
+                      <div class="form-group">
+                        <label for="exampleFormControlInput1">Meeting Title</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Insert title of meeting here" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Agenda</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Insert agenda of meeting here" required></textarea>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlSelect1">Meeting options</label>
+                        <select class="form-control" id="exampleFormControlSelect1" required>
+                          <option>Formal</option>
+                          <option>Informal</option>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlSelect1">Select date</label>
+                        <select class="form-control" id="exampleFormControlSelect1" required>
+                          <option>Date 1</option>
+                          <option>Date 2</option>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlInput1">Participant</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter participant" required>
+                      </div>
+                      <button type="submit" class="btn btn-success"><right>Create</right></button>
+                    </form>
                 </div>
             </div>
         </div>
